e2c75e0e7db3d2b1d01ccce5a188abf46b45822a
Miku <mia.nakano@icloud.com>
2022-04-22 19:20:02 +0200

    add .gitignore, setup.py and requirements.txt, remove main.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='MariaDB-SQLBuilder',
+    version='0.0.1',
+    packages=[''],
+    package_dir={'': 'mariadb_sqlbuilder'},
+    url='https://github.com/princessmiku/MariaDB-SQLBuilder',
+    license='MIT',
+    author='Miku',
+    author_email='',
+    description='MariaDB SQL Builder is a simple and thread safe way to use SQL. Use your own SQL or use the integrated SQL Builder tool.',
+    python_requires='>=3.6.0',
+)
