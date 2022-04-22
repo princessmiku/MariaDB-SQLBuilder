da7ceca3ef1bb87082b89acee0094e06bd783c23
Miku <mia.nakano@icloud.com>
2022-04-22 21:49:20 +0200

    other try fix

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import pathlib
 from setuptools import setup
+
 HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
@@ -9,12 +10,11 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     version='0.0.1',
-    package_dir={'': 'mariadb_sqlbuilder'},
     packages=["mariadb_sqlbuilder"],
     url='https://github.com/princessmiku/MariaDB-SQLBuilder',
     license='MIT',
     author='Miku',
     author_email='',
     description='MariaDB SQL Builder is a simple and thread safe way to use SQL. Use your own SQL or use the integrated SQL Builder tool.',
-    python_requires='>=3.6.0',
+    python_requires='>=3.6.0'
 )
