0b0a4ece2fcea71c85caa3054740807d7a8daf00
Miku <mia.nakano@icloud.com>
2022-08-25 20:08:25 +0200

    implements json support in updateBuilder.py, remove in insertBuilder.py

--- mariadb_sqlbuilder/builder/updateBuilder.py
+++ mariadb_sqlbuilder/builder/updateBuilder.py
@@ -1,5 +1,7 @@
-from typing import Union
+from json import dumps
+from typing import Union, Dict, List
 
+from .dummy import TableBuilder
 from ..execution import executeFunctions
 from .baseBuilder import ConditionsBuilder, _getTCN, _transformValueValid
 from .joinBuilder import BaseJoinExtension
@@ -13,6 +15,7 @@ class UpdateBuilder(ConditionsBuilder, BaseJoinExtension):
         # check if variable already exists, else init it
         self.__toSet = {}
         self.sureNotUseConditions = False
+        self.__subSets = []
 
     def set(self, column, value: Union[str, int, None]):
         self.__toSet[_getTCN(self.tb.table, column)] = _transformValueValid(value)
@@ -34,14 +37,40 @@ class UpdateBuilder(ConditionsBuilder, BaseJoinExtension):
             cursor,
             self.get_sql()
         )
+        if self.__subSets:
+            for s in self.__subSets:
+                executeFunctions.execute(cursor, s.get_sql())
         cursor._connection.commit()
         self.tb.connect.makeCursorAvailable(cursor)
         return result
 
     def get_sql(self) -> str:
-        return f"UPDATE {self.tb.table} " \
+        sql = f"UPDATE {self.tb.table} " \
                f"{' '.join(self._joins) if self._joins else ''} " \
                f"SET " \
             f"{', '.join(['%s = %s' % (key, value) for (key, value) in self.__toSet.items()])} " \
             f"{self._getWhereSQL()};"
+        return sql
 
+    def set_json(self, json: Dict[str, any], join: Union[bool, List[str]] = False, pop: List[str] = None):
+        """
+        Set values with a json, don't forget where
+        :param json: dict with data example from select
+        :param join: should if join in join tables and edit the data? Set specific keys of the json or use a detection,
+        but there will be problems if you have json formats in individual keys that are not intended for join.
+        :param pop: pop keys from the json, if you have joins in select that not should insert
+        :return:
+        """
+        key: str
+        value: any
+        for key, value in zip(json.keys(), json.values()):
+            if isinstance(value, dict):
+                if isinstance(join, list):
+                    if key in join: self.__subSets.append(UpdateBuilder(TableBuilder(self.tb.connect, key)).set_json(value))
+                    else: self.set(key, dumps(value))
+                else:
+                    if join: self.__subSets.append(UpdateBuilder(TableBuilder(self.tb.connect, key)).set_json(value))
+                    else: self.set(key, dumps(value))
+            else:
+                self.set(key, value)
+        return self
