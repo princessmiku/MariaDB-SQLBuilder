a554621f4856638d78ac453889b65f384261048c
Mia <mia.nakano@icloud.com>
2023-02-02 17:05:12 +0100

    change setup version from 0.5.1 to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-VERSION = '0.5.2'
+VERSION = '1.0.0'
 
 setup(
     name='MariaDB-SQLBuilder',
