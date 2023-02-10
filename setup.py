4a6b6eba018eaefbd9b5d460cedda2033697aa22
Mia <mia.nakano@icloud.com>
2023-02-10 17:41:21 +0100

    set version to alpha

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-VERSION = '1.0.0'
+VERSION = '1.0.0a1'
 
 setup(
     name='MariaDB-SQLBuilder',
