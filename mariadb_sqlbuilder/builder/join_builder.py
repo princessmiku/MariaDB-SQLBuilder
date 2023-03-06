bdbfd9b05c805fb38cf7b4cdd2fa47d8a6f46b43
Olivier Locard <develocard@gmail.com>
2023-03-06 18:16:23 +0100

    Fix W0611: Unused ABCMeta imported from abc (unused-import)

--- mariadb_sqlbuilder/builder/join_builder.py
+++ mariadb_sqlbuilder/builder/join_builder.py
@@ -3,7 +3,7 @@
     https://mariadb.com/kb/en/joining-tables-with-join-clauses/
 
 """
-from abc import ABC, abstractmethod, ABCMeta
+from abc import ABC, abstractmethod
 
 from .base_builder import _get_tcn
 
