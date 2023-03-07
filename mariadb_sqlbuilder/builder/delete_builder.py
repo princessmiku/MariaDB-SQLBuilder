e3b4af69b1f80e05c2b0aa9258c9f9dbec6a8139
Mia <mia.nakano@icloud.com>
2023-03-07 01:22:23 +0100

    add C0116 Missing function or method docstring in delete_builder.py

--- mariadb_sqlbuilder/builder/delete_builder.py
+++ mariadb_sqlbuilder/builder/delete_builder.py
@@ -8,10 +8,19 @@ class DeleteBuilder(ConditionsBuilder):
         self.sure_not_use_conditions = False
 
     def im_sure_im_not_use_conditions(self, im_sure: bool = True):
+        """
+        Sets the sure_not_use_conditions attribute to the provided boolean value.
+        :param im_sure:
+        :return:
+        """
         self.sure_not_use_conditions = im_sure
         return self
 
     def execute(self) -> bool:
+        """
+        Executes the DELETE SQL statement built by the builder.
+        :return:
+        """
         if not self._where_conditions and not self.sure_not_use_conditions:
             raise PermissionError('Delete Builder: You are not sure enough not to use where')
         cursor = self.tb.connect.get_available_cursor()
@@ -22,6 +31,10 @@ class DeleteBuilder(ConditionsBuilder):
         self.tb.connect.release_cursor(cursor)
 
     def get_sql(self) -> str:
+        """
+        Returns the DELETE SQL statement built by the builder as a string.
+        :return:
+        """
         return f"DELETE FROM {self.tb.table} " \
             f"{self._get_where_sql()};"
 
