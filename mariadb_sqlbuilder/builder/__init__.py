52dd03cf2b9bab680ae88c679eb3f39e5f6beaae
Mia <mia.nakano@icloud.com>
2023-03-07 01:46:04 +0100

    add C0116 Missing function or method docstring in __init__.py

--- mariadb_sqlbuilder/builder/__init__.py
+++ mariadb_sqlbuilder/builder/__init__.py
@@ -15,19 +15,50 @@ class TableBuilder:
         self.table = table
 
     def select(self, column: str, **kwargs) -> SelectBuilder:
+        """
+        Returns a SelectBuilder instance to build a SELECT query.
+        :param column:
+        :param kwargs:
+        :return:
+        """
         return SelectBuilder(self, column, **kwargs)
 
     def update(self, **kwargs) -> UpdateBuilder:
+        """
+        Returns an UpdateBuilder instance to build an UPDATE query.
+        :param kwargs:
+        :return:
+        """
         return UpdateBuilder(self, **kwargs)
 
     def insert(self, **kwargs) -> InsertBuilder:
+        """
+        Returns an InsertBuilder instance to build an INSERT query.
+        :param kwargs:
+        :return:
+        """
         return InsertBuilder(self, **kwargs)
 
     def upsert(self, **kwargs) -> UpsertBuilder:
+        """
+        Returns an UpsertBuilder instance to build an UPSERT query.
+        :param kwargs:
+        :return:
+        """
         return UpsertBuilder(self, **kwargs)
 
     def delete(self, **kwargs) -> DeleteBuilder:
+        """
+        Returns a DeleteBuilder instance to build a DELETE query.
+        :param kwargs:
+        :return:
+        """
         return DeleteBuilder(self, **kwargs)
 
     def exists(self, **kwargs) -> ExistsBuilder:
+        """
+        Returns an ExistsBuilder instance to check if a record exists in the table.
+        :param kwargs:
+        :return:
+        """
         return ExistsBuilder(self, **kwargs)
