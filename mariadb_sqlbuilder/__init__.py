bdb134bc820ea8da6a368a9d1c2f448d557a7ae1
Mia <mia.nakano@icloud.com>
2023-03-16 15:49:31 +0100

    License notes, add in the readme the 1.0.0 functions

--- mariadb_sqlbuilder/__init__.py
+++ mariadb_sqlbuilder/__init__.py
@@ -1,8 +1,25 @@
 """
-    Maria DB SQL Builder
+Copyright (c) 2023 Miku
 
-    - helpful links -
-    Join:
-    https://mariadb.com/kb/en/joining-tables-with-join-clauses/
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This library requires the MariaDB module to function.
+Please ensure that the module is installed before using this library.
+
+This library is an extension of the MariaDB module for Python. The MariaDB
+module is licensed under the General Public License (GPL) version 2 or later.
+Please see the MariaDB licensing terms for details:
+https://github.com/mariadb-corporation/mariadb-connector-python/blob/1.1/LICENSE
 """
 from .connector import Connector
